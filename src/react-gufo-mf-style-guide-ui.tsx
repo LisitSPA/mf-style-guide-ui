0283e5c2d3770e0982ebf2689aa8789f43436091
elii rivarola <erivarola@lisit.cl>
2024-08-05 15:47:27 -0300

    styles global

--- src/react-gufo-mf-style-guide-ui.tsx
+++ src/react-gufo-mf-style-guide-ui.tsx
@@ -1,3 +1,5 @@
 export { default as PageNotFound } from "./components/NotFound/components/PageNotFound";
 export { default as RoutesWithNotFound } from "./components/NotFound/RoutesWithNotFound";
 export { default as ErrorBoundary } from "./components/ErrorBoundary";
+
+import "./index.css";
